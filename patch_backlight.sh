d254b4c54f8cd04eac91a0832fe4a64bfd9352dd
RehabMan <racerrehabman@gmail.com>
2016-04-14 11:54:42 -0700

    enhance patch_backlight.sh to generate for both 0x710 and 0xad9 PWM max

--- patch_backlight.sh
+++ patch_backlight.sh
@@ -5,6 +5,29 @@
 uid=10
 vanilla=/System/Library/Extensions/AppleBacklight.kext
 
+ioreg -n IGPU -arxw0>/tmp/org.rehabman.gfx.plist
+if [ ! -s /tmp/org.rehabman.gfx.plist ]; then
+    ioreg -n GFX0 -arxw0>/tmp/org.rehabman.gfx.plist
+fi
+if [ ! -s /tmp/org.rehabman.gfx.plist ]; then
+    echo Error generating AppleBaclightInjector.kext; not able to determine graphics device!
+    exit 1
+fi
+
+devid=`/usr/libexec/PlistBuddy -c "Print :0:device-id" /tmp/org.rehabman.gfx.plist|xxd -p`
+devid=${devid:0:8}
+case $devid in
+    16010000 | 26010000 | 12010000 | 22010000 | 66010000 | 42000000 | 46000000)
+        pwmmax=0x710
+        ;;
+
+    *)
+        pwmmax=0xad9
+        ;;
+esac
+
+echo using Backlight_$pwmmax.plist, for device $devid
+
 ioreg -n AppleBacklightDisplay -arxw0>/tmp/org.rehabman.display.plist
 if [ ! -s /tmp/org.rehabman.display.plist ]; then
     ioreg -n AppleDisplay -arxw0>/tmp/org.rehabman.display.plist
@@ -15,10 +38,10 @@ if [ ! -s /tmp/org.rehabman.display.plist ]; then
 fi
 id=`/usr/libexec/PlistBuddy -c "Print :0:DisplayProductID" /tmp/org.rehabman.display.plist`
 id=`printf "F%02dT%04x" $uid $id`
-sed "s/F99T1234/$id/g" Backlight.plist >/tmp/org.rehabman.merge.plist
+sed "s/F99T1234/$id/g" Backlight_$pwmmax.plist >/tmp/org.rehabman.merge.plist
 
 if [ -d AppleBacklightInjector.kext ]; then rm -Rf AppleBacklightInjector.kext; fi
-cp -R /System/Library/Extensions/AppleBacklight.kext AppleBacklightInjector.kext
+cp -RX /System/Library/Extensions/AppleBacklight.kext AppleBacklightInjector.kext
 plist=AppleBacklightInjector.kext/Contents/Info.plist
 #/usr/libexec/PlistBuddy -c "Copy ':IOKitPersonalities:AppleIntelPanelA:ApplePanels' ':ApplePanelsBackup'" $plist
 /usr/libexec/PlistBuddy -c "Delete ':IOKitPersonalities:AppleIntelPanelA:ApplePanels'" $plist
