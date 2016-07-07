2a4273a5cc940d8711ea5d0c8b9d8925d5230643
RehabMan <racerrehabman@gmail.com>
2016-07-07 07:46:08 -0700

    change Skylake models to use KEY102

--- install_acpi.sh
+++ install_acpi.sh
@@ -354,7 +354,7 @@ case "$1" in
         $0 inst_lores $2
         $0 inst_batt_g3
         cp $BUILDDIR/SSDT-8x0-G3-Skylake.aml $EFIDIR/EFI/CLOVER/ACPI/patched
-        cp $BUILDDIR/SSDT-KEY87.aml $EFIDIR/EFI/CLOVER/ACPI/patched
+        cp $BUILDDIR/SSDT-KEY102.aml $EFIDIR/EFI/CLOVER/ACPI/patched
         cp $BUILDDIR/SSDT-USB-8x0-G3.aml $EFIDIR/EFI/CLOVER/ACPI/patched
         ls $EFIDIR/EFI/CLOVER/ACPI/patched
     ;;
@@ -362,7 +362,7 @@ case "$1" in
         $0 inst_lores $2
         $0 inst_batt_g3
         cp $BUILDDIR/SSDT-6x0-G2-Skylake.aml $EFIDIR/EFI/CLOVER/ACPI/patched
-        cp $BUILDDIR/SSDT-KEY87.aml $EFIDIR/EFI/CLOVER/ACPI/patched
+        cp $BUILDDIR/SSDT-KEY102.aml $EFIDIR/EFI/CLOVER/ACPI/patched
         #cp $BUILDDIR/SSDT-USB-6x0-G2.aml $EFIDIR/EFI/CLOVER/ACPI/patched
         ls $EFIDIR/EFI/CLOVER/ACPI/patched
     ;;
