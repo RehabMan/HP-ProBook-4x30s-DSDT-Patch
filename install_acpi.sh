bf0909cfb5486b3568c1c11d33c6323c38654cb1
RehabMan <racerrehabman@gmail.com>
2016-08-29 08:23:53 -0700

    use SSDT-BATT for Zbook G1 Haswell

--- install_acpi.sh
+++ install_acpi.sh
@@ -343,7 +343,6 @@ case "$1" in
     ;;
     install_ZBook_G1_haswell)
         $0 inst_lores $2
-        $0 inst_batt_g2
         cp $BUILDDIR/SSDT-ZBook-G2-Haswell.aml $EFIDIR/EFI/CLOVER/ACPI/patched
         cp $BUILDDIR/SSDT-KEY87.aml $EFIDIR/EFI/CLOVER/ACPI/patched
         cp $BUILDDIR/SSDT-USB-ZBook-G1.aml $EFIDIR/EFI/CLOVER/ACPI/patched
